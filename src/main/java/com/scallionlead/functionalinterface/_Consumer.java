873b018043d9a2c8aba5164ee131eb8b36dd3a05
xlogger <xloggerster@gmail.com>
2020-05-03 17:43:46 +0800

    Consumer Functional interface

---
+++ src/main/java/com/scallionlead/functionalinterface/_Consumer.java
@@ -0,0 +1,34 @@
+package com.scallionlead.functionalinterface;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.function.Consumer;
+
+public class _Consumer {
+    // An operation that accept one argument
+
+    public static void main(String[] args) {
+        Customer maria = new Customer("Maria", "00000");
+        greetCustomer(maria);
+
+        // Consumer Functional interface
+        greetCustomerConsumer.accept(maria);
+    }
+
+    static Consumer<Customer> greetCustomerConsumer = customer -> System.out.printf("Hello %s, thanks for registering with phone number %s\n", customer.getCustomerName(), customer.getCustomerPhoneNumber());
+
+    static void greetCustomer(Customer customer) {
+        System.out.printf("Hello %s, thanks for registering with phone number %s\n", customer.getCustomerName(), customer.getCustomerPhoneNumber());
+
+    }
+
+    @AllArgsConstructor
+    @ToString
+    @Getter
+    static class Customer {
+        private final String customerName;
+        private final String customerPhoneNumber;
+    }
+}
