0b48b40dc9b2ddfa7d3a57d328cdd3771f3d3ec9
xlogger <xloggerster@gmail.com>
2020-05-03 17:52:55 +0800

    Biconsumer

--- src/main/java/com/scallionlead/functionalinterface/_Consumer.java
+++ src/main/java/com/scallionlead/functionalinterface/_Consumer.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class _Consumer {
@@ -12,16 +13,30 @@ public class _Consumer {
     public static void main(String[] args) {
         Customer maria = new Customer("Maria", "00000");
         greetCustomer(maria);
+        greetCustomerV2(maria, false);
 
         // Consumer Functional interface
         greetCustomerConsumer.accept(maria);
+
+        // Biconsumer Functional interface
+        greetCustomerConsumerV2.accept(maria, false);
     }
+
+    // Biconsumer
+    static BiConsumer<Customer, Boolean> greetCustomerConsumerV2 = (customer, showPhoneNumber) ->
+              System.out.printf("Hello %s, thanks for registering with phone number %s\n",
+                    customer.getCustomerName(), showPhoneNumber ? customer.getCustomerPhoneNumber() : "******");
+
 
     static Consumer<Customer> greetCustomerConsumer = customer -> System.out.printf("Hello %s, thanks for registering with phone number %s\n", customer.getCustomerName(), customer.getCustomerPhoneNumber());
 
     static void greetCustomer(Customer customer) {
         System.out.printf("Hello %s, thanks for registering with phone number %s\n", customer.getCustomerName(), customer.getCustomerPhoneNumber());
+    }
 
+    static void greetCustomerV2(Customer customer, boolean showPhoneNumber) {
+        System.out.printf("Hello %s, thanks for registering with phone number %s\n",
+                customer.getCustomerName(), showPhoneNumber ? customer.getCustomerPhoneNumber() : "******");
     }
 
     @AllArgsConstructor
